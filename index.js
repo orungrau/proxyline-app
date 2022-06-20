7be8fb7550e8d439115810cfe4705e4e1021c930
Anatoly Myaskov <myaskov@me.com>
2022-06-20 22:38:49 +0300

    add husky; add format script; cleanup;

--- index.js
+++ index.js
@@ -4,6 +4,5 @@
 
 import {AppRegistry} from 'react-native';
 import {App} from './src/main';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('proxyline', () => App);
---
+++ src/pages/debug-page/debug-page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View} from 'react-native';
+import styles from './styles';
+
+interface Props {}
+
+const DebugPage: React.FC<Props> = ({}) => {
+  return <View style={styles.container} />;
+};
+
+export default DebugPage;
