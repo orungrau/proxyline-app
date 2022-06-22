e0061b5570cea0c33f1969f13ba9c91c306d4ba8
Anatoly Myaskov <myaskov@me.com>
2022-06-22 10:27:55 +0300

    add fonts; small fix;

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import codePush from 'react-native-code-push';
+import RNBootSplash from 'react-native-bootsplash';
 import {DebugPage} from '~pages/debug-page';
+import {StatusBar} from 'react-native';
+import {Colors} from '~styles';
 
 const App = () => {
-  return <DebugPage />;
+  useEffect(() => {
+    RNBootSplash.hide({fade: true}).then();
+  }, []);
+
+  return (
+    <>
+      <StatusBar translucent={true} backgroundColor={Colors.CLEAN} />
+      <DebugPage />
+    </>
+  );
 };
 
-let mainApp = codePush(App);
-if (__DEV__) {
-  mainApp = App;
-}
-export default mainApp;
+export default !__DEV__ ? codePush(App) : App;
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -2,3 +2,4 @@ export const PRIMARY = '#FAC637';
 export const PRIMARY_BACKGROUND = '#0F1218';
 export const WHITE = '#FFFFFF';
 export const BLACK = '#000000';
+export const CLEAN = '#00000000';
