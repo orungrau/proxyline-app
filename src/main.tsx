e65cbbf90f52fcec0b89670198eefe2896cf9557
Anatoly Myaskov <myaskov@me.com>
2022-07-10 15:27:25 +0300

    add black screen;

--- src/main.tsx
+++ src/main.tsx
@@ -1,77 +1,21 @@
 import React, {useEffect} from 'react';
 import codePush from 'react-native-code-push';
 import RNBootSplash from 'react-native-bootsplash';
-import {StatusBar, StyleSheet, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {Colors} from '~styles';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import ProxyLineSDK from '~libs/proxyline-sdk';
-import {PROJECT_ID, PROJECT_KEY} from '~constants';
-import {NavigationContainer, AuthNavigator} from '~navigations';
-import {Provider} from 'react-redux';
-import store, {persistor, useAppSelector} from '~stores';
-import {PersistGate} from 'redux-persist/integration/react';
-import {getGlobal} from '~stores/global/selectors';
-import {LoadingOverlay} from '~components/loading-overlay';
-import {getAccountSelector} from '~stores/account/selectors';
-import moment from 'moment';
-// @ts-ignore
-import localization from 'moment/locale/ru';
-import {WakeupPage} from '~pages/wakeup-page';
 
-moment.updateLocale('ru', localization);
-
-ProxyLineSDK.setProject(PROJECT_ID, PROJECT_KEY);
-
-const App = () => {
-  const account = useAppSelector(getAccountSelector());
-  const {loading} = useAppSelector(getGlobal());
-
+const ProvidedApp = () => {
   useEffect(() => {
     RNBootSplash.hide({fade: true}).then();
   }, []);
 
-  return (
-    <View style={styles.app}>
-      <StatusBar
-        translucent={true}
-        backgroundColor={Colors.CLEAN}
-        barStyle={'light-content'}
-      />
-      <NavigationContainer>
-        {account.isAuth ? <WakeupPage /> : <AuthNavigator />}
-      </NavigationContainer>
-      {loading ? (
-        <View style={styles.overlay}>
-          <LoadingOverlay />
-        </View>
-      ) : null}
-    </View>
-  );
-};
-
-const ProvidedApp = () => {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <SafeAreaProvider>
-          <App />
-        </SafeAreaProvider>
-      </PersistGate>
-    </Provider>
-  );
+  return <View style={styles.app} />;
 };
 
 const styles = StyleSheet.create({
   app: {
     flex: 1,
-    backgroundColor: Colors.PRIMARY_BACKGROUND,
-  },
-  overlay: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+    backgroundColor: Colors.BLACK,
   },
 });
 
